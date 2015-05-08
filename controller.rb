da4f6fc34551db5ad915d96bdbef1368cc1a4c19
hartnerk <hartnerk@gmail.com>
2015-05-08 16:51:25 -0400

    firstdraftroughworkingcode

--- controller.rb
+++ controller.rb
@@ -1,29 +1,34 @@
-require_relative "../stack.rb"
-require_relative "../parser.rb"
+require_relative "models/stack.rb"
+require_relative "parser.rb"
+require_relative "view.rb"
+require 'pry'
 
 class Controller
-	attr_reader :test_stack :file_name
-	def initialize(file_name)
-		@test_stack=Stack.new(Parser.load(file_name))
+	attr_reader :test_stack, :file_name
+	def initialize
+		@test_stack=Stack.new(Parser.load('cards.csv'))
 		run!
 	end
 
 	def look_at_cards
+		input=""
 		until input=="exit" || test_stack.stack_complete
 			View.display(test_stack.gets_card_q)
 			input=View.user_input
 			if test_stack.correct?(input)
 				View.correct
 				test_stack.tag_correct
-
 			else
 				View.wrong
 				input=View.user_input
 				if test_stack.correct?(input)
 					View.correct
 					test_stack.tag_correct
+					test_stack.next_card
 				else
 					View.wrong
+				end
+			
 			end
 			test_stack.next_card
 		end
@@ -34,14 +39,14 @@ class Controller
 		View.welcome
 		test_stack.shuffle
 		look_at_cards
-		
 		# View.done
 		# input=View.user_input
 		# if input=="continue"
 
 		# else
 		# puts number_wrong
-		Parser.save(file_name, test_stack.stack)
+		   binding.pry
+		Parser.save('cards.csv', test_stack.stack)
 
 		View.congrats
 	end
@@ -50,4 +55,4 @@ class Controller
 end
 
 
-Controller.new('cards.csv')
+Controller.new
